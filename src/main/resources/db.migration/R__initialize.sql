32d1afa2c5bb459ed7e67c05527f6f75462193f9
Piotr Berezowski <piotr.berezowski97@gmail.com>
2020-07-03 03:39:48 +0200

    Add auth_state table definition

--- src/main/resources/db.migration/R__initialize.sql
+++ src/main/resources/db.migration/R__initialize.sql
@@ -2,7 +2,19 @@ drop table IF EXISTS ffs_ca.users;
 
 create TABLE ffs_ca.users (
     id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL
+    username TEXT UNIQUE NOT NULL
 );
 
 insert into ffs_ca.users (username) values ('admin');
+
+drop table IF EXISTS ffs_ca.auth_state;
+
+create TABLE ffs_ca.auth_state (
+    token SERIAL PRIMARY KEY,  -- TODO - set to UUID
+    user_id REFERENCES ffs_ca.users (id) ON DELETE CASCADE NOT NULL,
+    start_time TIMESTAMP NOT NULL DEFAUTL NOW(),
+    reps INTEGER NOT NULL,
+    succ_tries INTEGER NOT NULL DEFAULT 0 CHECK (succ_tries >= 0),
+    curr_X INTEGER,
+    curr_A boolean ARRAY
+)
